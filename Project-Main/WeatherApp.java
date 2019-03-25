70464c4cc4bc755cbed8adb9432a68bd9fe1a995
danieljjyuen <45666484+danieljjyuen@users.noreply.github.com>
2019-03-25 18:23:45 -0400

    return user location(city) by ip address

--- Project-Main/WeatherApp.java
+++ Project-Main/WeatherApp.java
@@ -1,5 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import org.json.JSONObject;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 
 // example of using classes to handle the data for current weather
@@ -13,13 +15,43 @@ public class WeatherApp {
 
     public static void main(String[] args) throws IOException {
         
-        String apiKey = ""
+        String apiKey = "";
         
         CurrentWeather cw = new CurrentWeather("http://api.openweathermap.org/data/2.5/weather?q=new york&APPID="+apiKey);
-        CurrentWeather cw2 = new CurrentWeather("http://api.openweathermap.org/data/2.5/weather?q=chicago&APPID="+apiKey);        
+        CurrentWeather cw2 = new CurrentWeather("http://api.openweathermap.org/data/2.5/weather?q=chicago&APPID="+apiKey);
         System.out.println(cw.toString());
-        
+       // CurrentWeather fw = new CurrentWeather("http://api.openweathermap.org/data/2.5/forecast?q=new%20york,us&APPID="+apiKey);
 
+
+
+        //get public ip
+    String publicIpAddress = "";
+    try {
+        URL url_name = new URL("http://bot.whatismyipaddress.com");
+        BufferedReader readip = new BufferedReader(new InputStreamReader(url_name.openStream()));
+        //return address
+        publicIpAddress = readip.readLine().trim();
+        }
+    catch(Exception e)
+        {
+        publicIpAddress = "error";
+         }
+
+        //get city from ipaddress
+
+     String cityname="";
+        try {
+            URL url_name2 = new URL("https://ipapi.co/"+publicIpAddress+"/city");  //if you want to return zipcode replace city with "postal"
+            BufferedReader readcity = new BufferedReader(new InputStreamReader(url_name2.openStream()));
+            //return address
+            cityname = readcity.readLine().trim();
+        }
+        catch(Exception e)
+        {
+            cityname = "error";
+        }
+        System.out.println(cityname);
     }
-    
+
 }
+
