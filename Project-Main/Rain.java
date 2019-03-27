54142e08d9c9eb1f8780e535849b8ba4125cd0b9
Tommy Huang <48419590+thuang288@users.noreply.github.com>
2019-03-26 22:00:57 -0400

    Add files via upload
    Parsed API and got these classes

---
+++ Project-Main/Rain.java
@@ -0,0 +1,21 @@
+package com.codebeasty.json.json;
+
+public class Rain {
+    private String h;
+
+    public String get3h ()
+    {
+        return h;
+    }
+
+    public void set3h (String h)
+    {
+        this.h = h;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [3h = "+h+"]";
+    }
+}
