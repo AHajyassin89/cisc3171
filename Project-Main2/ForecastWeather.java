b193c9d52249c1a8d62d4f28337559c053bee1de
mbran1 <bm1208@bcmail.brooklyn.cuny.edu>
2019-04-11 11:36:57 -0400

    added arrays with daily temperature highs and lows. fixed bug of getting min temp for max temp

--- Project-Main2/ForecastWeather.java
+++ Project-Main2/ForecastWeather.java
@@ -1,17 +1,18 @@
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import java.io.IOException;
+import java.text.ParseException;
+import org.json.*;
 
 public class ForecastWeather {
 public Weather[] forecastList;
+private double[] highs;
+private double[] lows;
 //array to store weather objects
 
     public ForecastWeather() {
 
     }
 
-    public ForecastWeather(String url) throws IOException {
+    public ForecastWeather(String url) throws IOException, ParseException {
         JSONObject forecastWeather = JSON.readJsonFromUrl(url);
         JSONArray weathers = forecastWeather.getJSONArray("list");
 
@@ -24,13 +25,47 @@ public Weather[] forecastList;
 
             this.forecastList[i].setDate(list.getString("dt_txt"))  ;
             this.forecastList[i].setCloudDescription(cloudInfo.getString("description"));
-            this.forecastList[i].setTemp((list.getJSONObject("main").getDouble("temp") - 273.15) * 9 / 5 + 32);
+            this.forecastList[i].setTemp(list.getJSONObject("main").getDouble("temp"));
             this.forecastList[i].setHumidity(list.getJSONObject("main").getDouble("humidity"));
-            this.forecastList[i].setMaxTemp(((list.getJSONObject("main").getDouble("temp_max") - 273.15) * 9 / 5 + 32));
-            this.forecastList[i].setMinTemp((list.getJSONObject("main").getDouble("temp_min") - 273.15) * 9 / 5 + 32);   ;
+            this.forecastList[i].setMaxTemp(list.getJSONObject("main").getDouble("temp_max"));
+            this.forecastList[i].setMinTemp(list.getJSONObject("main").getDouble("temp_min"));
+
+
+
+	    // removed the math because you can add "&units=imperia" at the end of the url
+            //"http://api.openweathermap.org/data/2.5/forecast?q=new york&appid=2f872b5246cef2d7a2bfcf6e10a62160&lang=eng&units=imperial"
+            
+            
+
         }
-
-
+        highs = new double[5];
+            lows = new double [5];
+     
+            for (int i=0, j=1;i<(forecastList.length-1);i++)
+            {
+                if (j<6)
+                {
+                lows[j-1] = forecastList[i].getMinTemp();
+                highs[j-1] = forecastList[i].getMaxTemp();
+                while ( i<(forecastList.length-2) &&
+                        forecastList[i].getDateWithoutTime().compareTo(forecastList[i+1].getDateWithoutTime()) == 0)
+                        
+                {
+                    if(lows[j-1] > forecastList[i+1].getMinTemp())
+                    {
+                        lows[j-1] = forecastList[i+1].getMinTemp();
+                    }
+                    if(highs[j-1] < forecastList[i+1].getMaxTemp())
+                    {
+                        highs[j-1] = forecastList[i+1].getMaxTemp();
+                    }
+                    i++;
+                }
+                j++;
+                }
+            }
     }
+    
 
+    
 }
--- Project-Main2/Weather.java
+++ Project-Main2/Weather.java
@@ -1,14 +1,31 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+
+
 public class Weather {
     private String cloudDescription;
     private double temp;
     private double humidity;
     private double maxTemp;
     private double minTemp;
-    private String date;
+    private Date date;
+    
+    private final SimpleDateFormat sdf;
+    
+    
 
-    Weather(){}
+    Weather(){
+        sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        
+    }
 //weather object class to hold the information from json
 
+    
+    
     public void setCloudDescription(String cloudDescription){this.cloudDescription = cloudDescription;}
     public String getCloudDescription() { return this.cloudDescription; }
 
@@ -16,7 +33,7 @@ public class Weather {
     public double getTemp() { return this.temp; }
 
     public void setMaxTemp(double maxTemp){this.maxTemp=maxTemp;}
-    public double getMaxTemp() { return this.minTemp;  }
+    public double getMaxTemp() { return this.maxTemp;  }
 
     public void setMinTemp(double minTemp){this.minTemp = minTemp;}
     public double getMinTemp() { return this.minTemp; }
@@ -24,12 +41,24 @@ public class Weather {
     public void setHumidity(double humidity){this.humidity= humidity;}
     public double getHumidity() {  return this.humidity; }
 
-    public void setDate(String date){this.date = date;}
-    public String getDate() {  return this.date; }
-
+    public void setDate(String date) throws ParseException{this.date = sdf.parse(date);}
+    public String getDate() {  return this.date.toString(); }
+    public Date getDateAsDate() {  return this.date; }
+    
+    public Date getDateWithoutTime() throws ParseException 
+        { 
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+            return formatter.parse(formatter.format(this.date)); 
+        }
+    
+    
     @Override
     public String toString() {
         return ("Date: " + date + "\nTemperature: " + temp + "\nMaximum Temperature: " + maxTemp + "\nMinimum"
                 + " Temperature: " + minTemp + "\nHumidity: " + humidity + "\n" + cloudDescription);
     }
+    
+    
+
+    
 }
